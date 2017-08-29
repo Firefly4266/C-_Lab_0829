3166d8555c0a8174ed9e3280a781bb68e22abded
Firefly4266 <gaines.johnp@gmail.com>
2017-08-29 14:13:07 -0700

    Partial Interface completed

---
+++ Insurance/IPolicy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Insurance
+{
+    interface IPolicy
+    {
+        int Id { get; }
+        float PropertyLimit { get; set; }
+        float LiabilityLimit { get; set; }
+        TimeSpan CoverageStart(TimeSpan start);
+        TimeSpan CoverageEnded(TimeSpan end);
+    }
+}
