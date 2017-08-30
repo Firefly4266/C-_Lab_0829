96f3a56bacdc89f0b04e79e061cda43528d97aef
Firefly4266 <gaines.johnp@gmail.com>
2017-08-29 21:57:32 -0700

    Added Interfaces

---
+++ Insurance/Policy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Insurance
+{
+    public class Policy
+    {
+        private ArrayList accounts;
+        private int nextid = 1;
+        public Policy() { }
+        //void Started(TimeSpan startDate);
+        //void Ended(TimeSpan endDate);
+
+        string Status { get; }
+        int Id { get; }
+        decimal PropertyLimit { get; }
+        decimal LiabilityLimit { get; }
+    }
+
+}
---
+++ Insurance/PolicyDefinitions.cs
@@ -0,0 +1,15 @@
+﻿//PolicyDefinitions.cs
+
+using System;
+
+interface IStructure
+{
+    string DwellingType { get; }
+    string ParcelId { get; }
+    int PostalCode { get; }
+}
+interface IWheels
+{
+    string Vin { get; }
+    string VehicleType { get; }
+}
